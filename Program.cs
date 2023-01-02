9f248b2ad1df96f85647870e8c084d2e7f8a72dc
Dina Filippova <dinafilippova@gmail.com>
2023-01-02 18:07:18 +0700

    Task68

--- Program.cs
+++ Program.cs
@@ -3,12 +3,13 @@ int n = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число m: ");
 int m = Convert.ToInt32(Console.ReadLine());
 
-Akkermans(n, m);
+Console.WriteLine(Akkermans(n, m));
 
 int Akkermans (int x, int y)
 {
-if (x == 0) return y + 1;
-if (x != 0 && y == 0)  return Akkermans(y - 1, 1);
-if (x > 0 && y > 0) return Akkermans(x - 1, Akkermans(x, y - 1));
-return Akkermans(x,y);
+int result = 0;
+if (x == 0) result = y + 1;
+if (x != 0 && y == 0)  result = Akkermans(y - 1, 1);
+if (x > 0 && y > 0) result = Akkermans(x - 1, Akkermans(x, y - 1));
+return result;
 }
