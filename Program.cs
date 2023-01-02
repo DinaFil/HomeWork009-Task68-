f9c67cbe8585b4ebb38e82cef2b35e16e57a417f
Dina Filippova <dinafilippova@gmail.com>
2023-01-02 17:30:40 +0700

    Task68

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Akkermans(n, m);
+
+int Akkermans (int x, int y)
+{
+if (x == 0) return y + 1;
+if (x != 0 && y == 0)  return Akkermans(y - 1, 1);
+if (x > 0 && y > 0) return Akkermans(x - 1, Akkermans(x, y - 1));
+return Akkermans(x,y);
+}
